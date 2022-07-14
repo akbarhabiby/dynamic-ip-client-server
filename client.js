fe36f4bf8467e7ee7b7af806a4ed8c753f84fc73
Muhammad Akbar Habiby Khalid <akbarhabiby@icloud.com>
2022-07-14 19:55:42 +0700

    Added client

---
+++ client.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const client = (method = "GET", url = "", headers) => {
+  return new Promise((resolve) => {
+    let idx = url.indexOf("/");
+    let hostname = url;
+    let path = "/";
+    let port;
+
+    if (idx !== -1) {
+      hostname = url.slice(0, url.indexOf("/"));
+      url.slice(url.indexOf("/"));
+    }
+
+    idx = url.indexOf(":");
+
+    if (idx !== -1 && url.slice(url.indexOf(":") + 1).length >= 4) {
+      hostname = url.slice(0, url.indexOf("/") + 1);
+      port = +url.slice(idx + 1, idx + 5);
+    }
+
+    let data = [];
+    http
+      .request(
+        {
+          method,
+          hostname,
+          port,
+          path,
+          headers,
+        },
+        (res) => {
+          res.on("data", (c) => data.push(c));
+          res.on("end", () => {
+            try {
+              resolve(JSON.parse(Buffer.concat(data).toString()));
+            } catch (e) {
+              resolve(Buffer.concat(data).toString());
+            }
+          });
+        }
+      )
+      .end();
+  });
+};
+
+((port = 8888) => {
+  setInterval(async () => {
+    try {
+      const ip = await client("GET", "ifconfig.me");
+      const result = await client("POST", `127.0.0.1:${port}`, {
+        "x-request": `{"aXBhZGRy": "${ip}"}`,
+      });
+      console.log(result);
+    } catch (e) {
+      console.error(e);
+    }
+  }, 300000);
+})();

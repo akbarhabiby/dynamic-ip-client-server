b5de80122209e25d662fcdd1c8bf1c9a0eab3a96
Muhammad Akbar Habiby Khalid <akbarhabiby@icloud.com>
2022-07-14 20:19:06 +0700

    Fixed bug on hostname

--- client.js
+++ client.js
@@ -14,7 +14,7 @@ const client = (method = "GET", url = "", headers) => {
     idx = url.indexOf(":");
 
     if (idx !== -1 && url.slice(url.indexOf(":") + 1).length >= 4) {
-      hostname = url.slice(0, url.indexOf("/") + 1);
+      hostname = url.slice(0, url.indexOf(":"));
       port = +url.slice(idx + 1, idx + 5);
     }
 
